3b3c51b5dd2ccfe5b6a45290be9f2e6bb41d96b2
AndreVsc <vsandre40@gmail.com>
2023-10-18 18:12:07 -0300

    Curso0.0.0.1v

---
+++ Curso/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Security;
+using System.Security.Permissions;
+using System.Text;
+
+namespace System{
+    public class Program{
+        private static void Main(string[]   args){
+
+        }
+    }
+}
